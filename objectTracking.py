5263209e6bf9f4ad673311986bcde11531e7922f
Василий Лопаткин <loopatkin.2002@outlook.com>
2019-09-23 22:23:10 +0700

    first commit

---
+++ objectTracking.py
@@ -0,0 +1,65 @@
+import random
+
+class ObjectTracking(object) :
+
+    def __init__(self, classID):
+        self.classID = classID
+        self.probability = 0.1
+        self.bbox = []
+        self.id = 0
+
+
+    def createNewID(self, bbox, allObjects):
+        id = random.randint(1,9999)
+        unicID = False
+
+        while unicID != True and len(allObjects) != 0:
+            for object in allObjects:
+                if object.id == id:
+                    id = random.randint(1,9999)
+                    unicID = False
+                else:
+                    unicID = True
+
+        self.id = id
+        self.bbox = bbox
+
+    def tracking(self, newbbox, allObjects):
+        for object in allObjects:
+            if object.id == self.id:
+                oldx = range(self.bbox[0], self.bbox[2])
+                oldy = range(self.bbox[1], self.bbox[3])
+                newx = range(newbbox[0], newbbox[2])
+                newy = range(newbbox[1], newbbox[3])
+                xintersection = []
+                yintersection = []
+
+                for pixel in newx:
+                    if pixel in oldx:
+                        xintersection.append(pixel)
+
+                for pixel in newy:
+                    if pixel in oldx:
+                        yintersection.append(pixel)
+
+                if len(xintersection) != 0 and len(yintersection) != 0:
+                    sqold = (self.bbox[2] - self.bbox[0]) * (self.bbox[3] - self.bbox[1])
+                    xintersectionMin = min(xintersection)
+                    yintersectionMin = min(yintersection)
+                    xintersectionMax = max(xintersection)
+                    yintersectionMax = max(yintersection)
+                    sqintersection = (xintersectionMax - xintersectionMin) * (yintersectionMax - yintersectionMin)
+
+                    k = sqintersection / sqold
+
+                    if k >= 0.15:
+                        self.probability += 0.1
+                        return 0
+                    else:
+                        self.probability -= 0.1
+                        if self.probability == 0 or self.probability <= 0:
+                            deletObject()
+
+
+    def deletObject(self):
+        return self.id
